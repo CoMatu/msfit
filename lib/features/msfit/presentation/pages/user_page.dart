8eaa03bdda8235fceda330cde9d244ce530cf38b
CoMatu <matusevichvv@gmail.com>
2019-10-08 13:02:07 +0500

    в работе анимация круга на странице пользователя

--- lib/features/msfit/presentation/pages/user_page.dart
+++ lib/features/msfit/presentation/pages/user_page.dart
@@ -2,9 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'dart:math';
 
-class UserPage extends StatelessWidget {
+class UserPage extends StatefulWidget {
+  @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController progressController;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    progressController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1000));
+    animation = Tween(begin: 0.0, end: 30.0).animate(progressController)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    progressController.forward();
+
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
       children: <Widget>[
@@ -60,7 +84,7 @@ class UserPage extends StatelessWidget {
         ),
         Center(
           child: CustomPaint(
-            foregroundPainter: CircleProgress(40),
+            foregroundPainter: CircleProgress(animation.value),
             child: Container(
               height: 200.0,
               width: 200.0,
@@ -106,19 +130,19 @@ class CircleProgress extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round;
 
-    Offset center = Offset(size.width/2, size.height/2);
-    double radius = min(size.width/2, size.height/2) - 7;
+    Offset center = Offset(size.width / 2, size.height / 2);
+    double radius = min(size.width / 2, size.height / 2) - 7;
 
     canvas.drawCircle(center, radius, outerCircle);
 
-    double angle = 2 * pi * (currentProgress/100);
+    double angle = 2 * pi * (currentProgress / 100);
 
-    canvas.drawArc(Rect.fromCircle(center: center, radius: radius), - pi/2, angle, false, completeArc);
+    canvas.drawArc(Rect.fromCircle(center: center, radius: radius), -pi / 2,
+        angle, false, completeArc);
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
-    return null;
+    return true;
   }
 }
