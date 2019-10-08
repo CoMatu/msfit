f9093e7cc1c23c1a69d753dd35185524269fb0a1
CoMatu <matusevichvv@gmail.com>
2019-10-08 16:57:27 +0500

    added opacity

--- lib/features/msfit/presentation/pages/user_page.dart
+++ lib/features/msfit/presentation/pages/user_page.dart
@@ -14,6 +14,8 @@ class _UserPageState extends State<UserPage>
   Animation animation;
   Animation animationInner;
 
+  bool _visible = false;
+
   @override
   void initState() {
     super.initState();
@@ -22,106 +24,111 @@ class _UserPageState extends State<UserPage>
     progressInnerController = AnimationController(
         vsync: this, duration: Duration(milliseconds: 1000));
     animation = Tween(begin: 0.0, end: 30.0).animate(progressController);
-    animationInner = Tween(begin: 0.0, end: 80.0).animate(progressInnerController);
+    animationInner = Tween(begin: 0.0, end: 100.0).animate(progressInnerController);
 
     animation.addListener(() {
       setState(() {});
     });
 
     animationInner.addListener(() {
-      setState(() {});
+      setState(() {
+        _visible = true;
+      });
     });
-
     progressInnerController.forward();
     progressController.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 30.0),
-                child: Column(
-                  children: <Widget>[
-                    Opacity(
-                      opacity: 0.6,
-                      child: Container(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          'Today, April 19',
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.w500),
+    return AnimatedOpacity(
+      duration: Duration(milliseconds: 500),
+      opacity: _visible ? 1.0 : 0.0,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Column(
+                    children: <Widget>[
+                      Opacity(
+                        opacity: 0.6,
+                        child: Container(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            'Today, April 19',
+                            style: TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.w500),
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        'hello, Sarah',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 26.0,
-                            fontWeight: FontWeight.w600,
-                            letterSpacing: 1.0),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'hello, Sarah',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 1.0),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 30.0),
-              child: Container(
-                height: 60.0,
-                width: 60.0,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  image: DecorationImage(
-                      image: ExactAssetImage('res/images/user_face_blue.jpg'),
-                      fit: BoxFit.scaleDown),
-                  borderRadius: BorderRadius.circular(60.0),
-                  border: Border.all(width: 4.0, color: Colors.white),
+              Padding(
+                padding: const EdgeInsets.only(right: 30.0),
+                child: Container(
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    image: DecorationImage(
+                        image: ExactAssetImage('res/images/user_face_blue.jpg'),
+                        fit: BoxFit.scaleDown),
+                    borderRadius: BorderRadius.circular(60.0),
+                    border: Border.all(width: 4.0, color: Colors.white),
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: CustomPaint(
-              foregroundPainter:
-                  CircleProgress(animation.value, animationInner.value),
-              child: Container(
-                height: 180.0,
-                width: 180.0,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      '14',
-                      style: TextStyle(
-                          fontSize: 60.0,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    Text(
-                      'Today, April 19',
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.w500),
-                    ),
-                  ],
+              )
+            ],
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: CustomPaint(
+                foregroundPainter:
+                    CircleProgress(animation.value, animationInner.value),
+                child: Container(
+                  height: 180.0,
+                  width: 180.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '14',
+                        style: TextStyle(
+                            fontSize: 60.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        'Today, April 19',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
